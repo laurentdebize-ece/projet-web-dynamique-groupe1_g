f0f1e0d620fd08c08a8728a97e40a3417c0338e3
Margaux Berdjah <margauxberdjah@gmail.com>
2023-05-12 13:17:08 +0200

    formulaire connexion

--- connexion/formulairedeconnexion.php
+++ connexion/formulairedeconnexion.php
@@ -15,12 +15,17 @@ if (isset($_POST['submit'])) {
     $query = "SELECT * FROM admin WHERE username = '$username' AND password = '$password'";
     $result = mysqli_query($conn, $query);
 
-    if (mysqli_num_rows($result) == 1) {
-        // Redirection vers la page d'accueil après connexion réussie
-        header('Location: page_accueil.html');
-        exit;
+    // Vérifier si la requête s'est exécutée avec succès
+    if ($result) {
+        if (mysqli_num_rows($result) == 1) {
+            // Redirection vers la page d'accueil après connexion réussie
+            header('Location: page_accueil.html');
+            exit;
+        } else {
+            echo "Email ou mot de passe incorrect";
+        }
     } else {
-        echo "Email ou mot de passe incorrect";
+        echo "Erreur lors de l'exécution de la requête : " . mysqli_error($conn);
     }
 }
 

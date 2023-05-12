aa6518bed345d5e903b8fe111ba5bca1dcb9c1ea
Margaux Berdjah <margauxberdjah@gmail.com>
2023-05-12 14:11:26 +0200

    la connexion marche!!

--- connexion/formulairedeconnexion.php
+++ connexion/formulairedeconnexion.php
@@ -4,7 +4,7 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbname = 'omnesmyskillsnew';
+$dbname = 'omnesmyskillsfinal';
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
 // Vérification des identifiants de connexion
@@ -12,14 +12,15 @@ if (isset($_POST['submit'])) {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    $query = "SELECT * FROM admin WHERE username = '$username' AND password = '$password'";
+    $query = "SELECT * FROM admin WHERE email = '$username' AND mdp = '$password'";
     $result = mysqli_query($conn, $query);
 
     // Vérifier si la requête s'est exécutée avec succès
     if ($result) {
         if (mysqli_num_rows($result) == 1) {
             // Redirection vers la page d'accueil après connexion réussie
-            header('Location: page_accueil.html');
+            header("Location: ../accueil/pageaccueil.html");
+            //echo "Email ou mot de passe correct";
             exit;
         } else {
             echo "Email ou mot de passe incorrect";

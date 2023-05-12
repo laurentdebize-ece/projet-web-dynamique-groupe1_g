3389287ffbf52955f454e69ca5c822c2ace98727
lena <lena.blampain@edu.ece.fr>
2023-05-12 13:59:19 +0200

    newbdd

--- omnesmyskillsfinal.sql
+++ omnesmyskillsfinal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : dim. 07 mai 2023 à 14:53
+-- Généré le : ven. 12 mai 2023 à 11:58
 -- Version du serveur : 5.7.39
 -- Version de PHP : 7.4.33
 
@@ -48,7 +48,7 @@ INSERT INTO `admin` (`nom`, `prenom`, `mdp`, `email`) VALUES
 --
 
 CREATE TABLE `classe` (
-  `numéro` int(50) NOT NULL,
+  `numeroclasse` int(50) NOT NULL,
   `promo` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -56,7 +56,7 @@ CREATE TABLE `classe` (
 -- Déchargement des données de la table `classe`
 --
 
-INSERT INTO `classe` (`numéro`, `promo`) VALUES
+INSERT INTO `classe` (`numeroclasse`, `promo`) VALUES
 (1, 2026),
 (2, 2026),
 (3, 2026);
@@ -78,12 +78,13 @@ CREATE TABLE `compétences` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `correction`
+-- Structure de la table `enseigner`
 --
 
-CREATE TABLE `correction` (
-  `emailprof` varchar(255) NOT NULL,
-  `numeval` int(50) NOT NULL
+CREATE TABLE `enseigner` (
+  `emailproff` varchar(255) NOT NULL,
+  `numeroclasse` int(50) NOT NULL,
+  `numeromatiere` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -94,7 +95,7 @@ CREATE TABLE `correction` (
 
 CREATE TABLE `matières` (
   `nom` varchar(200) NOT NULL,
-  `numeroclasse` int(50) NOT NULL,
+  `numeromatiere` int(50) NOT NULL,
   `volume horaire` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -102,11 +103,9 @@ CREATE TABLE `matières` (
 -- Déchargement des données de la table `matières`
 --
 
-INSERT INTO `matières` (`nom`, `numeroclasse`, `volume horaire`) VALUES
+INSERT INTO `matières` (`nom`, `numeromatiere`, `volume horaire`) VALUES
 ('Mathématiques', 1, 10),
 ('Informatique', 2, 12);
-('Electronique',3, 11)
-('Humanités',1,10)
 
 -- --------------------------------------------------------
 
@@ -115,8 +114,7 @@ INSERT INTO `matières` (`nom`, `numeroclasse`, `volume horaire`) VALUES
 --
 
 CREATE TABLE `niveval` (
-  `numeval` int(50) NOT NULL,
-  `id` int(50) NOT NULL
+  `numeval` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -169,8 +167,9 @@ INSERT INTO `étudiant` (`nom`, `prenom`, `emaileleve`, `mot de passe`, `numéro
 CREATE TABLE `évaluations` (
   `numeval` int(50) NOT NULL,
   `emaileleve` varchar(200) NOT NULL,
-  `nummatiere` int(50) NOT NULL,
-  `numniveval` int(50) NOT NULL
+  `numniveval` int(50) NOT NULL,
+  `email du prof` varchar(255) NOT NULL,
+  `id` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -187,7 +186,7 @@ ALTER TABLE `admin`
 -- Index pour la table `classe`
 --
 ALTER TABLE `classe`
-  ADD PRIMARY KEY (`numéro`);
+  ADD PRIMARY KEY (`numeroclasse`);
 
 --
 -- Index pour la table `compétences`
@@ -196,24 +195,24 @@ ALTER TABLE `compétences`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `correction`
+-- Index pour la table `enseigner`
 --
-ALTER TABLE `correction`
-  ADD KEY `fk_emailprof` (`emailprof`),
-  ADD KEY `fk_numeroeval` (`numeval`);
+ALTER TABLE `enseigner`
+  ADD UNIQUE KEY `numeromatiere` (`emailproff`),
+  ADD KEY `fk_numéroclasse` (`numeroclasse`),
+  ADD KEY `fk_numeromatiere` (`numeromatiere`);
 
 --
 -- Index pour la table `matières`
 --
 ALTER TABLE `matières`
-  ADD PRIMARY KEY (`numeroclasse`);
+  ADD PRIMARY KEY (`numeromatiere`);
 
 --
 -- Index pour la table `niveval`
 --
 ALTER TABLE `niveval`
-  ADD PRIMARY KEY (`numeval`),
-  ADD KEY `fk_id` (`id`);
+  ADD PRIMARY KEY (`numeval`);
 
 --
 -- Index pour la table `professeur`
@@ -234,38 +233,35 @@ ALTER TABLE `étudiant`
 ALTER TABLE `évaluations`
   ADD PRIMARY KEY (`numeval`),
   ADD KEY `fk_emaileleve` (`emaileleve`),
-  ADD KEY `fk_numeromatiere` (`nummatiere`),
-  ADD KEY `fk_numniveval` (`numniveval`);
+  ADD KEY `fk_numniveval` (`numniveval`),
+  ADD KEY `fk_id` (`id`),
+  ADD KEY `fk_emailduprof` (`email du prof`);
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `correction`
+-- Contraintes pour la table `enseigner`
 --
-ALTER TABLE `correction`
-  ADD CONSTRAINT `fk_emailprof` FOREIGN KEY (`emailprof`) REFERENCES `professeur` (`emailprof`),
-  ADD CONSTRAINT `fk_numeroeval` FOREIGN KEY (`numeval`) REFERENCES `évaluations` (`numeval`);
-
---
--- Contraintes pour la table `niveval`
---
-ALTER TABLE `niveval`
-  ADD CONSTRAINT `fk_id` FOREIGN KEY (`id`) REFERENCES `compétences` (`id`);
+ALTER TABLE `enseigner`
+  ADD CONSTRAINT `fk_emailprof` FOREIGN KEY (`emailproff`) REFERENCES `professeur` (`emailprof`),
+  ADD CONSTRAINT `fk_numeromatiere` FOREIGN KEY (`numeromatiere`) REFERENCES `matières` (`numeromatiere`),
+  ADD CONSTRAINT `fk_numéroclasse` FOREIGN KEY (`numeroclasse`) REFERENCES `classe` (`numeroclasse`);
 
 --
 -- Contraintes pour la table `étudiant`
 --
 ALTER TABLE `étudiant`
-  ADD CONSTRAINT `fk_numeroclasse` FOREIGN KEY (`numéro de la classe`) REFERENCES `classe` (`numéro`);
+  ADD CONSTRAINT `fk_numeroclasse` FOREIGN KEY (`numéro de la classe`) REFERENCES `classe` (`numeroclasse`);
 
 --
 -- Contraintes pour la table `évaluations`
 --
 ALTER TABLE `évaluations`
+  ADD CONSTRAINT `fk_emailduprof` FOREIGN KEY (`email du prof`) REFERENCES `professeur` (`emailprof`),
   ADD CONSTRAINT `fk_emaileleve` FOREIGN KEY (`emaileleve`) REFERENCES `étudiant` (`emaileleve`),
-  ADD CONSTRAINT `fk_numeromatiere` FOREIGN KEY (`nummatiere`) REFERENCES `matières` (`numeroclasse`),
+  ADD CONSTRAINT `fk_id` FOREIGN KEY (`id`) REFERENCES `compétences` (`id`),
   ADD CONSTRAINT `fk_numniveval` FOREIGN KEY (`numniveval`) REFERENCES `niveval` (`numeval`);
 COMMIT;
 

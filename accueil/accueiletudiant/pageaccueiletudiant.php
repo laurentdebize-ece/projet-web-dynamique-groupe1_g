2bb4ed6e6afc86dc30e2682418bd962d2efe8f9d
ClaraJacquot <92568996+ClaraJacquot@users.noreply.github.com>
2023-05-15 21:31:04 +0200

    php

--- accueil/accueiletudiant/pageaccueiletudiant.php
+++ accueil/accueiletudiant/pageaccueiletudiant.php
@@ -10,5 +10,9 @@ $conn = mysqli_connect($host, $user, $pass, $dbname);
 
 $competences = $bdd->query(’SELECT nom FROM competences’);
 
+<p>
+Nom : <?php echo $competences[’Nom’]; ?>,<br>
+</p>
+
 $competences->closeCursor();
 ?>

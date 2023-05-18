eaec26f80c5cee4c6296e1655dee693c358e158a
ClaraJacquot <92568996+ClaraJacquot@users.noreply.github.com>
2023-05-18 16:06:44 +0200

    competence

--- accueil/accueiletudiant/pageaccueiletudiant.php
+++ accueil/accueiletudiant/pageaccueiletudiant.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Omnes MySkills - Accueil</title>
+  <link rel="stylesheet" type="text/css" href="pageaccueiletudiant.css">
+  
+</head>
 <?php
 $host = 'localhost';
 $user = 'root';
@@ -5,16 +14,83 @@ $pass = '';
 $dbname = 'omnesmyskillsfinal';
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
-
-
-$competences = $conn->query('SELECT nom FROM competences');
-echo 
-for each($competences to $competences)
-
-while ($row = $competences->fetch_assoc()) {
-    echo '<p>Nom : ' . $row['nom'] . ',<br></p>';
-}
-
-$competences->free_result();
-$conn->close();
+$requete = mysqli_query($conn,' SELECT nom FROM competences ');
+    
 ?>
+<body>
+  <header>
+  
+    <div class="flex-container">
+      <div><a href="#">Accueil</a></div>
+      <div><a href="http://localhost:8000/laurentdebize-ece/projet-web-dynamique-groupe1_g/accueil/accueiletudiant/matieresetudiant/matieres.html">Matière</a></div>
+      <div><a href="http://localhost:8000/laurentdebize-ece/projet-web-dynamique-groupe1_g/accueil/accueiletudiant/mescompetences/mescompetences.html">Mes Compétences</a></div>
+      <div><a href="#">Compétences Transverses</a></div>
+      <div><a href=" ../toutescompetences/toutescompetences.html">Toutes les compétences</a></div>
+    </div>
+    <div class="flex-container1">
+      <div class="right"><a href="#">Mon Compte étudiant</a></div>
+    </div>
+  </header>
+  <main>
+    <h1>Bienvenue sur Omnes MySkills</h1>
+    <div class="carousel">
+      <img src="lyon1.jpg" class="slide active" />
+      <img src="lyondenuit.png" class="slide" />
+      <img src="lyon2.jpg" class="slide" />
+      <div class="controls">
+        <span id="prev">&lt;</span>
+        <span id="next">&gt;</span>
+      </div>
+    </div>
+
+    <h2> Ici, vous pouvez trouver les différentes compétences pour vous améliorer</h2>
+    <h3>Compétences populaires</h3>
+    
+      <table>
+        <label>Nom de la compétence:</label>
+        <?php while ($donnees= mysqli_fetch_assoc($requete)){
+         ?>
+            <tr> 
+                <td> 
+                    <?php echo $donnees['nom']; ?> </td>
+        </tr>
+        <?php } ?>
+        </table>
+        
+    <h3>Dernières compétences ajoutées</h3>
+
+    <table>
+      <tr>
+        <td> Compétence 4</td>
+        <td> vvbfb </td>
+        <td> vfbrbr </td>
+
+      </tr>
+      <tr>
+        <td> Compétence 5 </td>
+        <td> bbrbrfe </td>
+        <td> grvefe </td>
+
+      </tr>
+      <tr>
+        <td> Compétence 6 </td>
+        <td> feefev </td>
+        <td> vrvrrb </td>
+      </tr>
+    </table>
+  </main>
+
+
+  <footer>
+    <p>Copyright © 2023 Omnes MySkills.</p>
+  </footer>
+  
+  <script src="pageaccueiletudiant.js"></script>
+</body>
+<?php    
+
+$requete->closeCursor(); ?>
+
+</html>
+
+

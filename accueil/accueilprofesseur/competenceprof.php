0c8930f06d1f92e6b9d8731c184e13b5a44ee3a0
ClaraJacquot <92568996+ClaraJacquot@users.noreply.github.com>
2023-05-23 12:01:54 +0200

    modifier competences

--- accueil/accueilprofesseur/competenceprof.php
+++ accueil/accueilprofesseur/competenceprof.php
@@ -14,36 +14,77 @@ $ecole = isset($_POST["ecole"]) ? $_POST["ecole"] : "";
 
 //*************************************
 // si bouton1 est cliqué
-if (isset($_POST["button1"])) {
-    $sql = "SELECT * FROM competences WHERE id='$id'";
-    $result = mysqli_query($conn, $sql);
-    //regarder s'il y a des resultats
-    if (mysqli_num_rows($result) == 0) {
-        echo "<p>La compétence n'a pas été trouvé.</p>";
-    } else {
-        echo "<table border='1'>";
-        echo "<tr>";
-        echo "<th>" . "id" . "</th>";
-        echo "<th>" . "nom" . "</th>";
-        echo "<th>" . "datecreation" . "</th>";
-        echo "<th>" . "datelimite" . "</th>";
-        echo "<th>" . "statut" . "</th>";
-        echo "<th>" . "ecole" . "</th>";
-        //afficher le resultat
-        while ($data = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . $data['id'] . "</td>";
-            echo "<td>" . $data['nom'] . "</td>";
-            echo "<td>" . $data['datecreation'] . "</td>";
-            echo "<td>" . $data['datelimite'] . "</td>";
-            echo "<td>" . $data['statut'] . "</td>";
-            echo "<td>" . $data['ecole'] . "</td>";
+
+if (isset($_POST["button1"])){
+?>
+    <!DOCTYPE html>
+<html>
+
+<head>
+    <title>Modifier</title>
+    <body>
+    <form action="competenceprof.php" method="post">
+
+        <table class="competences-table">
+            <tr>
+                <td size="20">Id</td>
+                <td><input type="number" name="id" size="60" required ></td>
+            </tr>
+            <tr>
+                <td>Nom</td>
+                <td><input type="text" name="nom" size="60"></td>
+            </tr>
+            <tr>
+                <td>Date de création</td>
+                <td><input type="date" name="datecreation" size="60"></td>
+            </tr>
+            <tr>
+                <td>Date limite</td>
+                <td><input type="date" name="datelimite" size="60"></td>
+            </tr>
+            <tr>
+                <td>Statut</td>
+                <td><input type="text" name="statut" size="60"></td>
+            </tr>
+            <tr>
+                <td>Ecole</td>
+                <td><input type="text" name="ecole" size="60" value="ECE"></td>
+            </tr>
+            <tr>
+                <td colspan="2" center="center">
+                    <input type="submit" name="valider" value="Valider">
+                </td>
+            </tr>
+        </table>
+    </form>
+        
+
+    </form>
+</body>
+
+</html>
+
+<?php } 
+if (isset($_POST["valider"])) {
+    if ($conn) {
+        $sql = "SELECT * FROM competences WHERE id ='$id'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $sql = "UPDATE competences SET nom='$nom',datecreation='$datecreation', datelimite='$datelimite', statut='$statut', ecole='$ecole' WHERE id='$id'";
+            $result=mysqli_query($conn, $sql);
+
+            if($result){
+                echo "<p>Modification réussi.</p>";
+            }else {
+                echo "<p>Erreur.</p>";
+            }
+        }else{
+            echo "<p>La compétence n'existe pas. Vous ne pouvez donc pas la modifier</p>";
+
         }
-        echo "</table>";
     }
-} else {
-    echo "<p>La compétence n'existe pas.</p>";
-}
+} 
 //end 
 //***********************************
 //si le bouton2 est cliqué
@@ -110,7 +151,7 @@ if (isset($_POST["button3"])) {
         } else {
             $sql = "DELETE FROM competences WHERE id = '$id'";
             $result = mysqli_query($conn, $sql);
-            
+
             if ($result) {
                 echo "Vous avez supprimé une compétences";
             } else {
@@ -121,3 +162,4 @@ if (isset($_POST["button3"])) {
 }
 
 mysqli_close($conn);
+?>
